66ff17543ff8cb16acacb245face8f07b1fcbfa1
vmbansal2001 <vmbansal2001@gmail.com>
2025-12-14 19:47:05 +0530

    feat: update hero section layout and styling
    - Changed the height of the hero section from 500px to full screen for improved visual impact.
    - Updated the heading style to uppercase for better emphasis on the property name.

--- views/project/[slug]/index/hero-section.tsx
+++ views/project/[slug]/index/hero-section.tsx
@@ -14,9 +14,9 @@ const HeroSection = () => {
         overlayOpacity={0.6}
         alt="landing-hero"
       >
-        <div className="w-screen h-[500px] flex items-center justify-between flex-col text-white px-4 sm:px-6">
+        <div className="w-screen h-screen flex items-center justify-between flex-col text-white px-4 sm:px-6">
           <PropertyNavbar property={property} />
-          <h1 className="text-center justify-start text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-light">
+          <h1 className="text-center justify-start text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-light uppercase">
             {property.name}
           </h1>
           <div className="h-16" />
