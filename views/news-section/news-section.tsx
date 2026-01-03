f52cd82ba03b0275723607531a8b675f71229b6b
vmbansal2001 <vmbansal2001@gmail.com>
2026-01-04 02:41:21 +0530

    feat: add logo to news section and update text styles
    - Integrated a new logo into the NewsSection component for enhanced branding visibility.
    - Updated heading and paragraph text styles to improve readability and visual appeal, including color adjustments and padding enhancements.

--- views/news-section/news-section.tsx
+++ views/news-section/news-section.tsx
@@ -1,6 +1,7 @@
 import { getNews } from "./news-section.utils";
 import SanityImage from "@/components/sanity-image";
 import Link from "next/link";
+import Logo from "@/assets/icons/mapsko-logo.svg";
 
 type Props = {};
 
@@ -11,13 +12,15 @@ const NewsSection = async (props: Props) => {
     <section className="bg-stone-50">
       <div className="common-frame-box py-12 md:py-16 lg:py-20 xl:py-28">
         <div className="flex flex-col items-center justify-center space-y-4 sm:space-y-5 md:space-y-6 pb-12 sm:pb-16 md:pb-20 lg:pb-24">
-          <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold uppercase text-center">
+          <Logo className="w-10 sm:w-12 md:w-14" />
+          <h2 className="text-sky-700 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold uppercase text-center px-4">
             Latest News
           </h2>
-          <p className="text-center text-sm sm:text-base md:text-lg font-light uppercase">
+          <p className="text-center text-neutral-500 text-sm sm:text-base md:text-lg font-light uppercase px-4">
             Stay updated with the latest news and updates from Mapsko.
           </p>
         </div>
+
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
           {news.map((item) => {
             return (
